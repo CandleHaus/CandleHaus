37b10c7b3bf3406b88d69ee5b9d0ce5fa419f3b8
CandleHaus <oscarcarlson34@gmail.com>
2026-05-17 16:05:20 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "res.cloudinary.com" }
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.printify.com" },
+      { protocol: "https", hostname: "images-api.printify.com" },
+      { protocol: "https", hostname: "storage.googleapis.com" },
     ]
   }
 };
